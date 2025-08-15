187e002795df990447faf04a0d787165940ac3d4
arodoo <haro.em@hotmail.com>
2025-08-14 21:59:44 -0600

    feat: initialize poetry-frontend with Vite and React setup

---
+++ poetry-backend/src/test/java/com/poetry/poetry_backend/PoetryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poetry.poetry_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoetryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
