9b002b6ac24baa33b7c1d6d5cdd0d7ea90d89635
arodoo <haro.em@hotmail.com>
2026-03-01 10:25:17 -0600

    feat: Add email normalization test for user registration and initialize mobile project dependencies.

--- poetry-backend/src/test/java/com/poetry/poetry_backend/interfaces/v1/auth/AuthEmailNormalizationTest.java
+++ poetry-backend/src/test/java/com/poetry/poetry_backend/interfaces/v1/auth/AuthEmailNormalizationTest.java
@@ -19,9 +19,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
